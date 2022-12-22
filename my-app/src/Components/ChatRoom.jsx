9cd931f6b50079527c959478dba0569b1300e5fb
Aniket Pilankar <aniketpilankar1998@gmail.com>
2022-12-23 00:52:03 +0530

    configure firebase with react

---
+++ my-app/src/Components/ChatRoom.jsx
@@ -0,0 +1,10 @@
+
+const ChatRoom = () => {
+  //   const signInWithGoogle = () => {
+  //     const provider = new firebase.auth.GoogleAuthProvider
+  //   };
+
+  //   return <button onClick={signInWithGoogle}>Sign In with Google</button>;
+  return <>ChatRoom</>;
+};
+export default ChatRoom;
---
+++ my-app/src/Components/SignIn.jsx
@@ -0,0 +1,9 @@
+// import firebase from "firebase/app";
+// import { initializeApp,firestore,auth } from "firebase/app";
+
+// const auth = firebase.auth;
+
+const SignIn = ({ signInWithGoogle }) => {
+  return <button onClick={signInWithGoogle}>Sign In with Google</button>;
+};
+export default SignIn;
